8fbf15efa6e2919bc3f14363e2c25f1591147946
Kanikaram-Kaushik <kanikaramkaushik@gmail.com>
2026-07-22 19:26:18 +0530

    Update VendorSidebar component

--- components/VendorSidebar.tsx
+++ components/VendorSidebar.tsx
@@ -34,7 +34,7 @@ const navItems = [
     ),
   },
   {
-    href: '/vendor/shop',
+    href: 'https://designbhk.com/',
     label: 'Shop',
     icon: (
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
